b80230b6a37489fb24406d52ca3e48c83425bde7
raydiak <raydiak@cyberuniverses.com>
2015-05-26 19:43:12 -0700

    Make sure we catch IO errors in from-file

--- lib/Upshift/Language.pm
+++ lib/Upshift/Language.pm
@@ -1,6 +1,9 @@
 unit role Upshift::Language[Grammar $grammar, Any $actions];
 
-method from-file (IO::Path:D $file) { self.from-string: $file.slurp.chomp }
+method from-file (IO::Path:D $file) {
+    use fatal;
+    self.from-string: $file.slurp.chomp;
+}
 
 method from-string (Str:D $str) {
     $grammar.parse($str, actions => $actions.new).?made //
